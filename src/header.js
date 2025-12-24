a038eb87360616ba248bc44a8d3b5d2897fb5486
Anano Topuria <anano.topuriaa@gmail.com>
2025-12-24 17:12:36 +0400

    style: add responsive design for main page

--- src/header.js
+++ src/header.js
@@ -1,38 +1,5 @@
-// export function renderHeader() {
-//   const headerRoot = document.querySelector("#site-header");
-//   if (!headerRoot) return;
-
-//   headerRoot.innerHTML = `
-//     <header>
-//       <div class="navigation-container">
-//         <a href="/" class="logo">
-//           <span>AnoMi</span>
-//         </a>
-
-//         <nav class="nav-menu">
-//           <ul>
-//             <li class="movieTab">Movies</li>
-//             <li class="tvTab">
-//                 Tv Series
-//             </li>
-//           </ul>
-//         </nav>
-
-//         <div class="search-wrapper">
-//           <input
-//             class="search-input"
-//             type="text"
-//             placeholder="Search movies..."
-//           />
-//         </div>
-//       </div>
-//     </header>
-//   `;
-// }
-
 import { searchMulti } from "./search/apiSearch";
 import { updateContentBySearch } from "./search/updateContentBySearch";
-// import { updateContent } from "./tabs/updateContent";
 
 export function renderHeader() {
   const headerRoot = document.querySelector("#site-header");
